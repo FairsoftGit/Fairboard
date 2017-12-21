d3e0c55633900200a3353f35eafcd212baa875be
JustinR <jpmremij@avans.nl>
2017-12-21 19:41:34 +0100

    setup
    gitignore en db connectie

--- DBConnection.php
+++ DBConnection.php
@@ -5,7 +5,7 @@ class DBConnection
     protected static $db;
     private function __construct() {
         try {
-            self::$db = new \PDO( 'mysql:host=localhost;dbname=fairsoft', 'root', '' );
+            self::$db = new \PDO( 'mysql:host=185.10.99.6:3306;dbname=sightvision_fairsoft', 'sightvision_fsUser', '82T0ki9di' );
             self::$db->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
         }
         catch (\PDOException $e) {

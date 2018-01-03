8011273200e9a59d6012906ac31175230b4f5144
JustinR <jpmremij@avans.nl>
2018-01-03 20:39:44 +0100

    Header fout opgelost
    Header fout opgelost

--- view/layout.php
+++ view/layout.php
@@ -19,7 +19,6 @@
             crossorigin="anonymous"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
             data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
@@ -29,16 +28,12 @@
     <a class="navbar-brand" href="\Fairboard">
         <img src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30"
              class="d-inline-block align-top" alt="">
-        <span class="menu-collapsed">Fairboard</span>
     </a>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
             <li class="nav-item active">
                 <a class="nav-link" href="\Fairboard">Home <span class="sr-only">(current)</span></a>
             </li>
-
-            <!-- This menu is hidden in bigger devices with d-sm-none.
-                 The sidebar isn't proper for smaller screens imo, so this dropdown menu can keep all the useful sidebar itens exclusively for smaller screens  -->
             <li class="nav-item dropdown d-sm-block d-md-none">
                 <a class="nav-link dropdown-toggle" href="#" id="smallerscreenmenu" data-toggle="dropdown"
                    aria-haspopup="true" aria-expanded="false">
@@ -48,26 +43,14 @@
                     <a class="dropdown-item" href="?controller=account&action=index">Accounts</a>
                     <a class="dropdown-item" href="?controller=product&action=index">Producten</a>
                 </div>
-            </li><!-- Smaller devices menu END -->
+            </li>
 
         </ul>
     </div>
-</nav><!-- NavBar END -->
-
-
-<!-- Bootstrap row -->
+</nav>
 <div class="row" id="body-row">
-    <!-- Sidebar -->
     <div id="sidebar-container" class="sidebar-expanded d-none d-md-block">
-        <!-- d-* hiddens the Sidebar in smaller devices. Its itens can be kept on the Navbar 'Menu' -->
-        <!-- Bootstrap List Group -->
         <ul class="list-group">
-            <!-- Separator with title -->
-            <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
-                <small>MAIN MENU</small>
-            </li>
-            <!-- /END Separator -->
-            <!-- Menu with submenu -->
             <a href="#submenu1" data-toggle="collapse" aria-expanded="false"
                class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
                 <div class="d-flex w-100 justify-content-start align-items-center">
@@ -76,7 +59,6 @@
                     <span class="submenu-icon ml-auto"></span>
                 </div>
             </a>
-            <!-- Submenu content -->
             <div id='submenu1' class="collapse sidebar-submenu">
                 <a href="?controller=account&action=index" class="list-group-item list-group-item-action bg-dark text-white">
                     <span class="menu-collapsed">Accounts</span>
@@ -85,40 +67,6 @@
                     <span class="menu-collapsed">Producten</span>
                 </a>
             </div>
-            <a href="#submenu2" data-toggle="collapse" aria-expanded="false"
-               class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-user fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Profile</span>
-                    <span class="submenu-icon ml-auto"></span>
-                </div>
-            </a>
-            <!-- Submenu content -->
-            <div id='submenu2' class="collapse sidebar-submenu">
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Settings</span>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Password</span>
-                </a>
-            </div>
-            <a href="#" class="bg-dark list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-tasks fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Tasks</span>
-                </div>
-            </a>
-            <!-- Separator with title -->
-            <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
-                <small>OPTIONS</small>
-            </li>
-            <!-- /END Separator -->
-            <a href="#" class="bg-dark list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-calendar fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Calendar</span>
-                </div>
-            </a>
         </ul><!-- List Group END-->
     </div><!-- sidebar-container END -->
 

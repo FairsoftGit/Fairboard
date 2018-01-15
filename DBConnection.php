4c6297f4a349e5c184b8f6501987efb56fb31609
Jeroen van der Hel <jeroenvdhel@gmail.com>
2018-01-15 15:05:10 +0100

    altered host db

--- DBConnection.php
+++ DBConnection.php
@@ -5,10 +5,8 @@ class DBConnection
     protected static $db;
     private function __construct() {
         try {
-            self::$db = new \PDO('mysql:host=178.251.31.13:3306;dbname=liannela_fairsoft', 'liannela_superuser', '6Z0wKRrPyg' );
-
-            //self::$db = new \PDO( 'mysql:host=185.10.99.6:3306;dbname=sightvision_fairsoft', 'sightvision_fsUser', '82T0ki9di' );
-            //self::$db = new \PDO( 'mysql:host=127.0.0.1:3306;dbname=fairsoft_2', 'root', '' );
+            self::$db = new \PDO('mysql:host=178.251.31.13:3306;dbname=liannela_fsdb', 'liannela_superuser', '6Z0wKRrPyg' );
+            //self::$db = new \PDO( 'mysql:host=127.0.0.1:3306;dbname=liannela_fairsoft', 'root', '' );
             self::$db->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
         }
         catch (\PDOException $e) {
